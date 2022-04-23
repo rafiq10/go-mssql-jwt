f3fa2a28a966b544cb17356e6ceaa3d65bc414b8
Rafal Bil <rafiq10@gmail.com>
2022-04-24 00:37:15 +0200

    log in complete with conditional login page

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -16,6 +16,13 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type LoginData struct {
+	LoggedIn bool
+	UsrName  string
+	ErrMsg   string
+	ErrCode  int
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
 	wd, err := os.Getwd()
 	if err != nil {
@@ -74,53 +81,59 @@ func Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func LogIn(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/html")
+
+	var d *LoginData
+	var tmpl *template.Template
+
 	if r.Method != http.MethodPost {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
+		d, tmpl = getLoginData(http.StatusMethodNotAllowed, "")
+		tmpl.Execute(w, d)
+		log.Fatalf("r.Method != http.MethodPost")
 	}
 
 	pwd := r.FormValue("pwd")
 	if pwd == "" {
-		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-		return
+		d, tmpl = getLoginData(http.StatusUnauthorized, "")
+		tmpl.Execute(w, d)
+		log.Fatalf("pwd empty")
 	}
 
 	tf := r.FormValue("tf")
 	if tf == "" {
-		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-		return
+		d, tmpl = getLoginData(http.StatusUnauthorized, "")
+		tmpl.Execute(w, d)
+		log.Fatalf("ptf empty")
 	}
 
 	db, err := mydb.GetDb()
 	if err != nil {
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
+		d, tmpl = getLoginData(http.StatusNotFound, "")
+		tmpl.Execute(w, d)
+		log.Fatalf("mydb.GetDb()=%v", err)
 	}
-
 	u := &models.User{}
 	u, err = u.GetByTF(db, tf)
 
 	if err != nil {
-		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-		return
+		d, tmpl = getLoginData(http.StatusUnauthorized, "")
+		tmpl.Execute(w, d)
+		log.Fatalf("u.GetByTF(db, tf)=%v", err)
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(u.Pwd), []byte(pwd))
 	if err != nil {
-		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		d, tmpl = getLoginData(http.StatusUnauthorized, "")
+		fmt.Printf("data: %v", d)
+		tmpl.Execute(w, d)
 		return
+		// log.Fatalf("bcrypt.CompareHashAndPassword([]byte(u.Pwd), []byte(pwd))=%v", err)
 	}
 
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	tmpl, err := template.ParseFiles(wd + "/templates/login.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tmpl.Execute(w, u)
+	d, tmpl = getLoginData(http.StatusOK, u.User_Name)
+	fmt.Printf("d: %v", d)
+	tmpl.Execute(w, d)
 }
 
 func JSONHandleError(w http.ResponseWriter, err error) {
@@ -144,4 +157,45 @@ func JSONError(w http.ResponseWriter, s int, errMsg string) error {
 	}
 	w.Write(o)
 	return nil
+}
+
+func getLoginData(statusCode int, userName string) (*LoginData, *template.Template) {
+	l := true
+	if statusCode != http.StatusOK {
+		l = false
+	}
+	d := &LoginData{
+		UsrName:  userName,
+		LoggedIn: l,
+		ErrMsg:   http.StatusText(statusCode),
+		ErrCode:  statusCode,
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		d = &LoginData{
+			UsrName:  userName,
+			LoggedIn: false,
+			ErrMsg:   http.StatusText(http.StatusInternalServerError),
+			ErrCode:  http.StatusInternalServerError,
+		}
+	}
+
+	tmpl, err := template.ParseFiles(wd + "/templates/login.html")
+	if err != nil {
+		d = &LoginData{
+			UsrName:  userName,
+			LoggedIn: d.LoggedIn,
+			ErrMsg:   http.StatusText(http.StatusInternalServerError),
+			ErrCode:  http.StatusInternalServerError,
+		}
+	} else {
+		d = &LoginData{
+			UsrName:  userName,
+			LoggedIn: d.LoggedIn,
+			ErrMsg:   http.StatusText(http.StatusOK),
+			ErrCode:  http.StatusOK,
+		}
+	}
+
+	return d, tmpl
 }
